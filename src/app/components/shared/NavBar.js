e9ceca79d9f0e0bbeca8e47c5d598692ea565b2a
Rasibul <rasibul179@gmail.com>
2023-12-06 22:06:41 +0600

    explore nextjs and setup nextjs,mui and create a navbar section

---
+++ src/app/components/shared/NavBar.js
@@ -0,0 +1,83 @@
+"use client"
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Image from 'next/image';
+import logo from '@/assets/logo.png'
+import { IconButton, Stack } from '@mui/material';
+// icons
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import Link from 'next/link';
+
+
+const navItems = [
+    {
+        route: 'Home',
+        path: '/'
+    },
+    {
+        route: 'Pages',
+        path: '/pages'
+    },
+    {
+        route: 'Category',
+        path: '/category'
+    },
+    {
+        route: 'About',
+        path: '/about'
+    },
+    {
+        route: 'Contact',
+        path: '/contact'
+    },
+];
+
+function NavBar() {
+
+    return (
+        <AppBar position="static" className='bg-black'>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Image src={logo} alt='logo' width={100} height={100} />
+                    <Box className="w-full text-center">
+                        {navItems.map((item) => (
+                            <Link key={item} href={item.path}>
+                                <Button className="text-white">
+                                    {item.route}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+                    <Box>
+                        <Stack direction="row" sx={{
+                            "& svg": {
+                                color: "white",
+                            },
+                        }}>
+                            <IconButton>
+                                <FacebookIcon />
+                            </IconButton>
+                            <IconButton>
+                                <TwitterIcon />
+                            </IconButton>
+                            <IconButton>
+                                <YouTubeIcon />
+                            </IconButton>
+                            <IconButton>
+                                <InstagramIcon />
+                            </IconButton>
+                        </Stack>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default NavBar;
